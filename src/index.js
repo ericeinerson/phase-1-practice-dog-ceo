10814c66271b119dd40efaf54b03132d523f95ad
karsyn ansari <karsyn.ansari@gmail.com>
2022-01-19 11:32:15 -0700

    what we tried

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('%c HI', 'color: firebrick')
+// console.log('%c HI', 'color: firebrick')
+//1. Fetch the dog images using a fetch request
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+fetch(imgUrl)
+  .then((response) => response.json())
+  .then(console.log)
+  .then((images) => {
+    images.forEach((image) => {
+      console.log
+      const dogImages = document.createElement("img");
+      console.log(dogImages);
+      // dogImages.src = images.message;
+    });
+  });
